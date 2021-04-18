7e0eff5d1e73fad378ff51ad2b4e16a2199ba9b4
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-04-18 16:02:56 +0300

    6.0.2. Заполнение массива. [#515828]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Alexander";
+        names[1] = "Sergey";
+        names[2] = "Stas";
+        names[3] = "Pavel";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
