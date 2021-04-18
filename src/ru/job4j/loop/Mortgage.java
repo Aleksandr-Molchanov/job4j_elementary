891f3651da30ee24b7af21bbec1bf80a88695998
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-04-18 14:23:10 +0300

    5.7. Ипотека [#515907]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,10 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double balance = amount + amount * percent / 100;
-        while (balance > 0) {
+        while (amount > 0) {
+            amount += amount * percent / 100;
+            amount -= salary;
             year++;
-            balance = balance - salary;
         }
         return year;
     }
