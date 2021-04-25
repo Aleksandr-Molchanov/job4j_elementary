5a66513c8a8a158926e269d5466e8b23b4d7222e
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-04-25 12:12:18 +0300

    1. Экзамен. Базовый синтаксис. [#515892]

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,30 +3,21 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        if (left.length == 0) {
-            rsl = right;
-        } else if (right.length == 0) {
-            rsl = left;
-        } else {
-            int leftIn = 0;
-            int rightIn = 0;
-            for (int index = 0; index < rsl.length && leftIn < left.length; index++) {
-                if (left[leftIn] <= right[rightIn]) {
-                    rsl[index] = left[leftIn];
-                    leftIn++;
-                } else {
-                    rsl[index] = right[rightIn];
-                    rightIn++;
-                }
-            }
-            for (int index = leftIn + rightIn; index < rsl.length; index++) {
-                if (leftIn == left.length) {
-                    rsl[index] = right[rightIn];
-                    rightIn++;
-                } else {
-                    rsl[index] = left[leftIn];
-                    leftIn++;
-                }
+        int leftIn = 0;
+        int rightIn = 0;
+        for (int index = 0; index < rsl.length; index++) {
+            if (leftIn == left.length) {
+                rsl[index] = right[rightIn];
+                rightIn++;
+            } else if (rightIn == right.length) {
+                rsl[index] = left[leftIn];
+                leftIn++;
+            } else if (left[leftIn] <= right[rightIn]) {
+                rsl[index] = left[leftIn];
+                leftIn++;
+            } else {
+                rsl[index] = right[rightIn];
+                rightIn++;
             }
         }
         return rsl;
